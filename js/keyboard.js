9c5dbf88ee23a97ed13af2cb986ef39a6e9d27c6
garradefogo124 <micaelmiranda124@gmail.com>
2022-10-28 16:34:55 -0300

    Estiilização do teclado

---
+++ js/keyboard.js
@@ -0,0 +1,55 @@
+const Keyboard = {
+    elements: {
+        main: null,
+        keysContainer: null,
+        keys: []
+    },
+
+    eventHandlers: {
+        oninput: null,
+        onclose: null
+    },
+
+    properties: {
+        value: "",
+        capsLock: false,
+    },
+
+    init() {
+        // Create main elements
+        this.elements.main = document.createElement("div")
+        this.elements.keysContainer = document.createElement("div")
+
+        // Setup main elements
+        this.elements.main.classList.add("keyboard", "1keyboard-hidden")
+        this.elements.keysContainer.classList.add("keyboard-keys")
+
+        // Add to DOM
+        this.elements.main.appendChild(this.elements.keysContainer)
+        document.body.appendChild(this.elements.main)
+    },
+
+    _createKeys() {
+
+    },
+
+    _triggerEvent(handlerName) {
+        console.log("Event Triggered, Event name: " + handlerName);
+    },
+
+    _toggleCapsLock() {
+        console.log("Caps Lock Toggled!")
+    },
+
+    open(initialValue, oninput, onclose) {
+
+    },
+
+    close() {
+
+    }
+}
+
+window.addEventListener("DOMContentLoaded", function () {
+    Keyboard.init();
+})
